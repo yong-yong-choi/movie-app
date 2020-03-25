df9693d38b126ac107daea148faf1aee7edf493d
yong <gotoplay000@gmail.com>
2020-03-26 02:09:54 +0900

    Updates

---
+++ precache-manifest.f49009807411ef79bc8e425f80274542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee424574146b02632c4426589de07a9",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "24df6c95969015247fbe",
+    "url": "/movie-app/static/css/main.523accdd.chunk.css"
+  },
+  {
+    "revision": "0149831013de804fa9b5",
+    "url": "/movie-app/static/js/2.d1705fb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.d1705fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24df6c95969015247fbe",
+    "url": "/movie-app/static/js/main.a34170c9.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  }
+]);
